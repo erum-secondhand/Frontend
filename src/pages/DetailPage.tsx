b45eeb97e2b11fab1fa27673f2591999cab9ec20
nagosu <nozick1021@gmail.com>
2024-02-20 17:18:38 +0900

    초기 세팅 완료

---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,5 @@
+function DetailPage() {
+  return <div className="">상세페이지임둥</div>;
+}
+
+export default DetailPage;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,5 @@
+function MainPage() {
+  return <div className="">메인페이지임둥</div>;
+}
+
+export default MainPage;
